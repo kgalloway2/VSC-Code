c929c763a8186146838b62b9a7f15c2824c18673
Keaton Galloway <keatongalloway@yahoo.com>
2021-05-20 12:54:29 -0500

    more julia work. made simple numberguess game java

--- java stuff/numberGuess.java
+++ java stuff/numberGuess.java
@@ -1,22 +1,28 @@
+import java.util.Scanner;
+
 public class numberGuess {
     int max;
     int min;
     int number;
     int numOfGuesses;
     
-    public numberGuess(int inputMax, int inputMin, int inputNumber) {
+    public numberGuess(int inputMin, int inputMax, int inputNumber) {
         max = inputMax;
         min = inputMin;
         number = inputNumber;
         numOfGuesses = 0;
+        System.out.println("Welcome to the number guessing game! Please guess a number between " + min + " and " + max + ".");
     }
 
-    public void checkGuess(int guess) {
+    public boolean checkGuess(int guess) {
+        boolean gameover = false;
         if (guess > max || guess < min) {
             System.out.println("Invalid guess");
         }
         else if (guess == number) {
-            System.out.println("Correct! You win!");
+            numOfGuesses += 1;
+            System.out.println("Correct! You won in only " + numOfGuesses + " guesses!");
+            gameover = true;
         }
         else if (guess < number) {
             System.out.println("Too low!");
@@ -26,9 +32,24 @@ public class numberGuess {
             System.out.println("Too high!");
             numOfGuesses += 1;
         }
+        return gameover;
+    }
+
+    public int takeGuess() {
+        System.out.println("What is your next guess?");
+        Scanner myObj = new Scanner(System.in);
+        int guess = Integer.parseInt(myObj.next());
+        return guess;
     }
 
     public static void main(String[] args) {
-
+        numberGuess newGame = new numberGuess(1, 100, 38);
+        while (true) {
+            int currentGuess = newGame.takeGuess();
+            boolean gameover = newGame.checkGuess(currentGuess);
+            if (gameover){
+                break;
+            }
+        }
     }
 }
