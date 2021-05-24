efc4107c5024ce1fe96263a8c96ca6b8288aae89
Keaton Galloway <keatongalloway@yahoo.com>
2021-05-24 13:28:09 -0500

    finished everything orginally planned on mtg app

--- mtg app/card_displayer.py
+++ mtg app/card_displayer.py
@@ -136,11 +136,40 @@ def begin_game(current_game_cards):
     # functions for ability list
 
     def ability_list_update(card):
-        ability_list.delete("1.0", "end")
+        ability_list.delete(0,'end')
         abilities = card["oracle_text"].split('\n')
-        for ability in abilities:
-            ability_list.insert('end', ability)
-            ability_list.insert('end', "\n\n")
+
+        for item in abilities:
+            ability_list.insert('end', item)
+
+    # functions for updating the stack
+
+    def add_ability_to_stack(card, ability):
+        new_insert = card + ": " + ability
+        the_stack.insert("1.0", "\n\n")
+        the_stack.insert("1.0", new_insert)
+
+    def delete_ability_from_stack():
+        the_stack.delete("1.0", "3.0")
+
+    def delete_entire_stack():
+        the_stack.delete("1.0", "end")
+
+    def add_multi_to_stack(card, ability,num_times):
+        for i in range(num_times):
+            new_insert = card + ": " + ability
+            the_stack.insert("1.0", "\n\n")
+            the_stack.insert("1.0", new_insert)
+
+    def add_card_to_stack(card):
+        the_stack.insert("1.0", "\n\n")
+        the_stack.insert("1.0", card)
+
+    def add_notes_to_stack(card, ability, notes):
+        new_insert = card + ": " + ability + " With notes: " + notes
+        the_stack.insert("1.0", "\n\n")
+        the_stack.insert("1.0", new_insert)
+
 
     # widgets for display
 
@@ -163,13 +192,49 @@ def begin_game(current_game_cards):
     show_pic_button.configure(command=lambda :show_picture(card_list.get(selection())))
     show_pic_button.place(x=630,y=1)
 
-    ability_list = Text(displayer)
+    ability_list = Listbox(displayer)
     ability_list.place(x=630,y=25)
-    ability_list['height'] = 20
-    ability_list['width'] = 20
-    ability_list['wrap'] = WORD
-    # ability_list.bind('<<ListBoxSelect>>', on_select)
+    ability_list['height'] = 6
+    ability_list['width'] = 120
+    ability_list.bind('<<ListBoxSelect>>', on_select)
     ability_list_update({"name": "Storm Crow", "scryfall_uri": "https://scryfall.com/card/9ed/100/storm-crow?utm_source=api", "image_uris": {"small": "https://c1.scryfall.com/file/scryfall-cards/small/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.jpg?1562730661", "normal": "https://c1.scryfall.com/file/scryfall-cards/normal/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.jpg?1562730661", "large": "https://c1.scryfall.com/file/scryfall-cards/large/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.jpg?1562730661", "png": "https://c1.scryfall.com/file/scryfall-cards/png/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.png?1562730661", "art_crop": "https://c1.scryfall.com/file/scryfall-cards/art_crop/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.jpg?1562730661", "border_crop": "https://c1.scryfall.com/file/scryfall-cards/border_crop/front/0/3/036ef8c9-72ac-46ce-af07-83b79d736538.jpg?1562730661"}, "mana_cost": "{1}{U}", "cmc": 2.0, "type_line": "Creature \u2014 Bird", "oracle_text": "Flying (This creature can't be blocked except by creatures with flying or reach.)", "power": "1", "toughness": "2", "colors": ["U"], "color_identity": ["U"], "keywords": ["Flying"], "rulings_uri": "https://api.scryfall.com/cards/036ef8c9-72ac-46ce-af07-83b79d736538/rulings"})
+
+    the_stack = Text(displayer)
+    the_stack.place(x=630,y=200)
+    the_stack['height'] = 28
+    the_stack['width'] = 90
+    the_stack['wrap'] = WORD
+
+    add_to_stack_button = Button(displayer, text='Add to stack.')
+    add_to_stack_button.configure(command= lambda :add_ability_to_stack(card_list.get(ACTIVE), ability_list.get(ACTIVE)))
+    add_to_stack_button.place(x=630, y = 130)
+
+    delete_from_stack_button = Button(displayer, text='Delete top ability from stack.')
+    delete_from_stack_button.configure(command=lambda :delete_ability_from_stack())
+    delete_from_stack_button.place(x=713, y = 130)
+
+    delete_entire_stack_button = Button(displayer, text='Clear the stack.')
+    delete_entire_stack_button.configure(command=lambda :delete_entire_stack())
+    delete_entire_stack_button.place(x=879, y = 130)
+
+    stack_info = Label(displayer, text="To remove an ability from the middle of the stack, highlight and delete it.")
+    stack_info.place(x=970, y=133)
+
+    add_multiple_to_stack_button = Button(displayer, text='Add to stack times: ')
+    add_multiple_to_stack_button.configure(command= lambda :add_multi_to_stack(card_list.get(ACTIVE), ability_list.get(ACTIVE), int(add_multiple_entry.get())))
+    add_multiple_to_stack_button.place(x=630, y = 160)
+    add_multiple_entry = Entry(displayer, width=3)
+    add_multiple_entry.place(x=748, y = 162)
+
+    add_card_to_stack_button = Button(displayer, text='Add card to stack.')
+    add_card_to_stack_button.configure(command= lambda :add_card_to_stack(card_list.get(ACTIVE)))
+    add_card_to_stack_button.place(x=775, y = 160)
+
+    add_notes_entry = Entry(displayer)
+    add_notes_entry.place(x=1015, y = 162)
+    add_notes_to_stack_button = Button(displayer, text='Add to stack with note: ')
+    add_notes_to_stack_button.configure(command= lambda :add_notes_to_stack(card_list.get(ACTIVE), ability_list.get(ACTIVE), add_notes_entry.get()))
+    add_notes_to_stack_button.place(x=880, y = 160)
 
     show_picture('default')
 
