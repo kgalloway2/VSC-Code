b064d1b1e7808eaedc5efcb25c1fc467836438ef
Keaton Galloway <keatongalloway@yahoo.com>
2021-05-26 13:22:53 -0500

    small changes before adding to website

--- julia stuff/password_generator.jl
+++ julia stuff/password_generator.jl
@@ -1,4 +1,4 @@
-characters = ['a','b','c','d','e'] #,'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function generator(length)
     password = ""
@@ -10,7 +10,7 @@ function generator(length)
 end
 
 randompasswords = []
-for i in 1:100
+for i in 1:10
     push!(randompasswords, generator(8))
 end
 
