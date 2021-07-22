980c190fac66f33eb5b53c89dc57ecf98317e6bc
Keaton Galloway <keatongalloway@yahoo.com>
2021-07-22 17:40:12 -0500

    more work on tab editor

--- java stuff/tabWriter.java
+++ java stuff/tabWriter.java
@@ -1,5 +1,5 @@
 // currently working for the single chord I have put in. It can display an example
-// still need to add all chords, fix spacing in display, add ability to add to tab
+// still need to add all chords, fix font in everything
 
 import java.awt.*;
 import java.awt.event.*;
@@ -23,33 +23,70 @@ public class tabWriter extends Frame{
     private String[] baseChords = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
     private String[] chordMods = {"Maj", "min", "Maj7", "7", "min7", "sus4"};
     private Hashtable<String, char[]> chordMatcher = new Hashtable<String, char[]>();
+    private String emptyMeasure = "e--------------\nB--------------\nG--------------\nD--------------\nA--------------\nE--------------";
 
 
     public tabWriter() {
         setLayout(new GridLayout(0, 3));
-        TextArea tab = new TextArea("Your tab here", 30, 50);
+        tab = new TextArea(emptyMeasure, 30, 50);
         Panel tabPanel = new Panel();
         tabPanel.add(tab);
         tabPanel.setSize(300,500);
         add(tabPanel);
 
-        TextArea lickDisplay = new TextArea("empty lick display",10,30);
+        addLick = new Button("Add Lick");
+        addLick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                //current stuff
+                String beforeTab = tab.getText();
+                String[] lines = beforeTab.split("\n");
+                int currentLength = lines.length;
+                // System.out.println(currentLength);
+                int currentLineLength = lines[currentLength - 1].length();
+                // System.out.println(currentLineLength);
+
+                // stuff to add
+                String newTab = lickDisplay.getText();
+                String[] newLines = newTab.split("\n");
+                int newLineLength = newLines[0].length();
+                if (currentLineLength + newLineLength >= 80) {
+                    tab.append(newTab);
+                } else {
+                    for (int i = 0; i <= 5; i++) {
+                        lines[currentLength - 6 + i] = lines[currentLength - 6 + i].concat(newLines[i].substring(1));
+                        // System.out.println("changed line to ");
+                        // System.out.println(lines[currentLength - 6 + i]);
+                    }
+                    String afterTab = "";
+                    
+                    for (String i : lines) {
+                        afterTab = afterTab.concat(i + "\n");
+                    }
+                    // System.out.println(afterTab);
+                    tab.setText(afterTab);
+                }
+            }
+        });
+
+        lickDisplay = new TextArea(emptyMeasure,10,30);
         Panel lickDisplayPanel = new Panel();
         lickDisplayPanel.add(lickDisplay);
+        lickDisplayPanel.add(addLick);
         lickDisplayPanel.setSize(50, 300);
         add(lickDisplayPanel);
 
         // create needed lists
-        List chords = new List(13);
+        chords = new List(13);
         for (String i : baseChords) {
             chords.add(i);
         }
-        List mods = new List(6);
+        mods = new List(6);
         for (String i : chordMods) {
             mods.add(i);
         }
-        Hashtable<String, Integer> lickMatcher = new Hashtable<String, Integer>();
-        List savedLicks = new List(3);
+        lickMatcher = new Hashtable<String, Integer>();
+        savedLicks = new List(3);
         for (int i = 0; i <= 2; i++) {
             lickMatcher.put(licks[i][0], i);
             savedLicks.add(licks[i][0]);
@@ -57,19 +94,19 @@ public class tabWriter extends Frame{
         char[] temp = {'0','3','2','0','1','0'};
         chordMatcher.put("CMaj", temp);
 
-        Button displayLickButton = new Button("Show lick with selected chord and modifiers");
+        displayLickButton = new Button("Show Selected Lick");
         displayLickButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
                 char[] currentChord = chordMatcher.get(chords.getSelectedItem().concat(mods.getSelectedItem()));
-                System.out.println(savedLicks.getSelectedItem());
-                System.out.println(lickMatcher.get(savedLicks.getSelectedItem()));
-                System.out.println(licks[lickMatcher.get(savedLicks.getSelectedItem())][0]);
+                // System.out.println(savedLicks.getSelectedItem());
+                // System.out.println(lickMatcher.get(savedLicks.getSelectedItem()));
+                // System.out.println(licks[lickMatcher.get(savedLicks.getSelectedItem())][0]);
 
                 String[] currentLick = Arrays.copyOfRange(licks[lickMatcher.get(savedLicks.getSelectedItem())], 1, 7);
-                for (int i = 0; i <= 5; i++) {
-                    System.out.println(currentLick[i]);
-                }
+                // for (int i = 0; i <= 5; i++) {
+                //     System.out.println(currentLick[i]);
+                // }
                 lickDisplay.setText(createChord(currentChord, currentLick));
             }
         });
