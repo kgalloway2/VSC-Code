d2cffcf4c094f255adf97564a2e036051bbd53fc
Keaton Galloway <keatongalloway@yahoo.com>
2021-08-05 10:02:41 -0500

    more work on pokedash

--- python stuff/pokeDash.py
+++ python stuff/pokeDash.py
@@ -1,10 +1,14 @@
 # add selector for displaying info on selected pokemon (a dropdown or maybe selected point)
+# try to fix data display table for selected pokemon
 # custom filter for data
 # use border in to see layout better
 # trendlines
+# maybe move all the styling to a stylesheet?
+# min and max height and width of elements for scaling purposes
 
 
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -41,7 +45,8 @@ stat_dicts = [{'label': "DFID", 'value': "DFID"},
             ]          
 
 
-app.layout = html.Div([
+app.layout = html.Div([    
+    
     html.Div([
         dcc.Dropdown(id='xstat-selector',
         options=stat_dicts,
@@ -65,20 +70,22 @@ app.layout = html.Div([
                 {'label': 'Mythical', 'value': '\"Mythical\"'},
                 {'label': 'Non-Legendary', 'value': '\"Non-Legendary\"'}
             ],
-            value=['\"Legendary\"', '\"Sub-Legendary\"', '\"Mythical\"', '\"Non-Legendary\"']
-        )
+            value=['\"Legendary\"', '\"Sub-Legendary\"', '\"Mythical\"', '\"Non-Legendary\"'],
+            style={'padding-bottom': '30px'}
+        ),
+        
+        html.Div([
+            html.Label(children="Only Fully-Evolved?", style={'text-align': 'center'}),
+            dcc.Slider(
+                id='evolved-filter',
+                min=0,
+                max=1,
+                marks={0: "No", 1: "Yes"}
+                )
+            ]
+        ),
     ], style={'width': '20%', 'display': 'inline-block'}),
-    
-    html.Div([
-        html.Label("Only Fully-Evolved?"),
-        dcc.Slider(
-            id='evolved-filter',
-            min=0,
-            max=1,
-            marks={0: "No", 1: "Yes"}
-        )
-    ], style={'width': '150px', 'display': 'inline-block'}
-    ),
+     
 
     html.Div([
         dcc.Checklist(
@@ -107,12 +114,23 @@ app.layout = html.Div([
     ], style={'width': '25%', 'display': 'inline-block'}),
 
     html.Div([
-        dcc.Graph(id='stat-graph')
+        dcc.Dropdown(id='poke-selector')
+    ]),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='stat-graph')
+        ], style={'width': '65%', 'display': 'inline-block'}),
+        html.Div([
+            # html.Label(id="poke-info", children="No Pokemon Selected")
+            dash_table.DataTable(id='poke-info')
+        ], style={'display': 'inline-block','width': '25%', 'float': 'top right', 'overflow': 'scroll', 'max-height': '300px', 'padding-bottom': '50px'})
     ])
 ])
 
 @app.callback(
     Output('stat-graph', 'figure'),
+    Output('poke-selector', 'options'),
     Input('xstat-selector', 'value'),
     Input('ystat-selector', 'value'),
     Input('legend-filter', 'value'),
@@ -133,11 +151,29 @@ def update_stat_graph(xstat, ystat, legend_filter, evolved_filter, gen_filter, t
     sec_type = current_df[current_df['Secondary.Type'].isin(type_filter)]
     current_df = pd.concat([prim_type, sec_type])
         
-
+    
+    options = [{'label': current_df.iat[i-1,4], 'value': current_df.iat[i-1,1]} for i in range(current_df.shape[0])]
     fig = px.scatter(current_df, x=xstat, y=ystat, hover_name=current_df['Pokemon.Name'], hover_data = [xstat, ystat] + data_list)
 
-    return fig
+    return fig, options
 
+@app.callback(
+    Output('poke-info', 'columns'),
+    Output('poke-info', 'data'),
+    Input('poke-selector', 'value')
+)
+def update_selected_poke(selected_poke):
+    data_df = poke_df[selected_poke - 1:selected_poke
+    ]
+    data_df = data_df.transpose()
+
+    # columns = [{"name": str(i), "id": str(i)} for i in data_df.columns]
+    columns = [{"name": 'Attributes', "id": '1'}, {"name": 'Pokemon', "id": '0'}]
+    print(columns)
+    data = data_df.to_dict('records')
+    print(data)
+
+    return columns, data
 
 if __name__ == '__main__':
     app.run_server(debug=True)
